0df8d110dae63bb349f61d8682e53d7638d8f3ef
Paul Kilmurray <paul@kilbot.com.au>
2014-11-12 23:32:49 +0800

    add locales to json

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,11 +4,14 @@ module.exports = function(grunt) {
     require('load-grunt-tasks')(grunt);
 
     grunt.registerTask('default', 'Package po & mo files', function() {
+        var locales = [];
 
         grunt.config('po2mo.files', { src: 'languages/*.po', expand: true});
 
         grunt.file.expand({ cwd: 'languages/' }, '*.po').forEach(function(po){
             var name = po.replace('.po', '');
+            locales.push( name.replace('woocommerce-pos-', '') );
+
             grunt.config('compress.' + name, {
                 options: {
                     archive: 'packages/' + name + '.zip'
@@ -26,6 +29,10 @@ module.exports = function(grunt) {
 
         grunt.task.run('po2mo');
         grunt.task.run('compress');
+
+        var pkg = grunt.file.readJSON('package.json');
+        pkg.locales = locales;
+        grunt.file.write('package.json', JSON.stringify(pkg, null, 2));
 
     });
 
